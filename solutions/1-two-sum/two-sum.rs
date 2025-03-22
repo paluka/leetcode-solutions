9a497f6f34b37fac6ea660f93cc91ec06ba477a9
Erik Paluka <erik.paluka@gmail.com>
2025-03-22 13:30:24 +0700

    chore(1. Two Sum): encapsulate the rust solution functions in a struct

--- solutions/1-two-sum/two-sum.rs
+++ solutions/1-two-sum/two-sum.rs
@@ -1,31 +1,35 @@
 use std::collections::HashMap;
 
-fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
-    let mut map: HashMap<i32, usize> = HashMap::new();
+struct Solution;
 
-    for (idx, value) in nums.iter().enumerate() {
-        let diff = target - value;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, usize> = HashMap::new();
 
-        if let Some(&diff_index) = map.get(&diff) {
-            return vec![diff_index as i32, idx as i32];
+        for (idx, value) in nums.iter().enumerate() {
+            let diff = target - value;
+
+            if let Some(&diff_index) = map.get(&diff) {
+                return vec![diff_index as i32, idx as i32];
+            }
+
+            map.insert(*value, idx);
         }
 
-        map.insert(*value, idx);
+        vec![]
     }
 
-    vec![]
-}
-
-fn two_sum1(nums: &Vec<i32>, target: i32) -> Vec<i32> {
-    for outer_idx in 0..nums.len() {
-        for inner_idx in (outer_idx + 1)..nums.len() {
-            if nums[outer_idx] + nums[inner_idx] == target {
-                return vec![outer_idx as i32, inner_idx as i32];
+    pub fn two_sum1(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for outer_idx in 0..nums.len() {
+            for inner_idx in (outer_idx + 1)..nums.len() {
+                if nums[outer_idx] + nums[inner_idx] == target {
+                    return vec![outer_idx as i32, inner_idx as i32];
+                }
             }
         }
-    }
 
-    vec![]
+        vec![]
+    }
 }
 
 fn main() {
@@ -33,8 +37,8 @@ fn main() {
     let nums1 = vec![2, 7, 11, 15];
     let target1 = 9;
     let expected_solution1 = vec![0, 1]; // Expected solution for nums1 and target1
-    let solution1_sum = two_sum(&nums1, target1);
-    let solution1_sum1 = two_sum1(&nums1, target1);
+    let solution1_sum = Solution::two_sum(nums1.clone(), target1);
+    let solution1_sum1 = Solution::two_sum1(nums1.clone(), target1);
 
     assert_eq!(expected_solution1, solution1_sum);
     assert_eq!(expected_solution1, solution1_sum1);
@@ -48,8 +52,8 @@ fn main() {
     let nums2 = vec![3, 2, 4];
     let target2 = 6;
     let expected_solution2 = vec![1, 2]; // Expected solution for nums2 and target2
-    let solution2_sum = two_sum(&nums2, target2);
-    let solution2_sum1 = two_sum1(&nums2, target2);
+    let solution2_sum = Solution::two_sum(nums2.clone(), target2);
+    let solution2_sum1 = Solution::two_sum1(nums2.clone(), target2);
 
     assert_eq!(expected_solution2, solution2_sum);
     assert_eq!(expected_solution2, solution2_sum1);
@@ -63,8 +67,8 @@ fn main() {
     let nums3 = vec![3, 3];
     let target3 = 6;
     let expected_solution3 = vec![0, 1]; // Expected solution for nums3 and target3
-    let solution3_sum = two_sum(&nums3, target3);
-    let solution3_sum1 = two_sum1(&nums3, target3);
+    let solution3_sum = Solution::two_sum(nums3.clone(), target3);
+    let solution3_sum1 = Solution::two_sum1(nums3.clone(), target3);
 
     assert_eq!(expected_solution3, solution3_sum);
     assert_eq!(expected_solution3, solution3_sum1);
@@ -78,8 +82,8 @@ fn main() {
     let nums4 = vec![1, 3, 4, 2];
     let target4 = 7;
     let expected_solution4 = vec![1, 2]; // Expected solution for nums4 and target4
-    let solution4_sum = two_sum(&nums4, target4);
-    let solution4_sum1 = two_sum1(&nums4, target4);
+    let solution4_sum = Solution::two_sum(nums4.clone(), target4);
+    let solution4_sum1 = Solution::two_sum1(nums4.clone(), target4);
 
     assert_eq!(expected_solution4, solution4_sum);
     assert_eq!(expected_solution4, solution4_sum1);
@@ -93,8 +97,8 @@ fn main() {
     let nums5 = vec![0, 4, 3, 0];
     let target5 = 0;
     let expected_solution5 = vec![0, 3]; // Expected solution for nums5 and target5
-    let solution5_sum = two_sum(&nums5, target5);
-    let solution5_sum1 = two_sum1(&nums5, target5);
+    let solution5_sum = Solution::two_sum(nums5.clone(), target5);
+    let solution5_sum1 = Solution::two_sum1(nums5.clone(), target5);
 
     assert_eq!(expected_solution5, solution5_sum);
     assert_eq!(expected_solution5, solution5_sum1);
